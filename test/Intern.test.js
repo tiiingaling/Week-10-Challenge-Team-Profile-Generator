49b3ae7d6ac11ec49d2d1dc7890a2988d7674aa2
tiiingaling <93777306+tiiingaling@users.noreply.github.com>
2023-02-04 12:29:10 +0000

    complete test files

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,19 +1,21 @@
 const Intern = require("../lib/Intern");
 
-describe('Employee', () => {
+describe('Intern', () => {
     it("Testing constructor", () =>{
 
-    const name = 'Ting'
+    const name = 'Ting';
     const id = 1;
-    const email = 'ting@bootcamp.com'
-    const role = 'Employee';
+    const email = 'ting@bootcamp.com';
+    const school = 'university of leicester';
+    const role = 'Intern';
 
-    const obj = new Employee(name, id, email);
+    const obj = new Intern(name, id, email, school);
 
     //tests that the above parameters match the class parameters
     expect(obj.name).toEqual(name);
     expect(obj.id).toEqual(id);
     expect(obj.email).toEqual(email);
+    expect(obj.school).toEqual(school);
 
     expect(obj.getName()).toEqual(name);
     expect(obj.getId()).toEqual(id);
--- test/Manager.test.js
+++ test/Manager.test.js
@@ -1,20 +1,22 @@
 const Manager = require("../lib/Manager");
 
-describe('Employee', () => {
+describe('Manager', () => {
     it("Testing constructor", () =>{
 
     const name = 'Ting'
     const id = 1;
     const email = 'ting@bootcamp.com'
-    const role = 'Employee';
+    const officeNumber = '1';
+    const role = 'Manager';
 
-    const obj = new Employee(name, id, email);
+    const obj = new Manager(name, id, email, officeNumber);
 
     //tests that the above parameters match the class parameters
     expect(obj.name).toEqual(name);
     expect(obj.id).toEqual(id);
     expect(obj.email).toEqual(email);
-
+    expect(obj.officeNumber).toEqual(officeNumber);
+    
     expect(obj.getName()).toEqual(name);
     expect(obj.getId()).toEqual(id);
     expect(obj.getEmail()).toEqual(email);
